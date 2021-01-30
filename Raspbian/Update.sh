034dbd769260760a5aca6147298a37fc02d655ca
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-01-29 23:19:22 -0400

    Update Update.sh

--- Raspbian/Update.sh
+++ Raspbian/Update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/Apagado.sh -q -O- | tr -d '\r' >/home/pi/Auto/Apagado.sh
-[ $? -eq 0 ] && echo "Version=1" > Current.ini && rm Update.sh
+[ $? -eq 0 ] && echo "Version=$1" > Current.ini && echo "Banca=$2" >> Current.ini && rm Update.sh
 exit
