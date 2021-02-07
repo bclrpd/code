e73e9fd35a36d918b4049ba06931bb89692135ca
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-02-07 17:45:34 -0400

    Update ControlHorario.sh

--- Ubuntu/ControlHorario.sh
+++ Ubuntu/ControlHorario.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 
-#$(</proc/uptime awk '{print int ($1)}') // tiempo encendida
+FECHA=1094993008
+BANCA=1475362797
+TURNO=1178772187 #("Entrada 1" o "Entrada 2")
+HORA=1549658220
+PUNTUAL=231354200 #(SI o NO)
+RAZON=430411897
+URL="https://docs.google.com/forms/d/e/1FAIpQLScaQs1b41h3mIVMsn6wNscjKLzeGVMXSc7xgD4W3VTh-mvtKw/formResponse"
+X=$(xdpyinfo | awk '/dimensions/{print $2}' | awk -F "x" '{print $1}')
+PX=$((($X-1078)/2))
+[ -f Registro ] || echo "  Fecha    HORA    T       Razon" > Registro
+[ -f Data.ini ] || echo "TIEMPO=0" > Data.ini
 . Data.ini
-[ $? -ne 0 ] && echo "TIEMPO=0" > Data.ini && . Data.ini
 ACUMULADO=${TIEMPO}
+
 while true ; do
 	TIME="$(cat </dev/tcp/time.nist.gov/13)"
 	if [[ "$TIME" == *"UTC(NIST)"* ]] ; then
@@ -12,120 +22,103 @@ while true ; do
 		Array=($TIME)
 		Hora=$(date --date "${Array[2]} today - 240 minutes" +%T)
 		HoraAbrio=$(date --date "$Hora today - $Segundos seconds" +%T)
-		Fecha=$(date -d"${Array[1]} ${Array[2]} today - 240 minutes" +%m/%d/%Y)	
-		Year=$(date --date "$Fecha" +%Y)
-		Mes=$(date --date "$Fecha" +%m)
-		Dia=$(date --date "$Fecha" +%d)		
-		
-		[ "$(xmlstarlet sel -t -v "count(/Data/Year[@ID=$Year])" Doc.xml)" -eq 0 ] && xmlstarlet ed -L \
-			-s /Data -t elem -n "YearTMP" -v "" \
-			-i //YearTMP -t attr -n "ID" -v "$Year" \
-			-r //YearTMP -v "Year" Doc.xml
-		[ "$(xmlstarlet sel -t -v "count(/Data/Year[@ID=$Year]/Mes[@ID=$Mes])" Doc.xml)" -eq 0 ] && xmlstarlet ed -L \
-			-s /Data/Year[@ID=$Year] -t elem -n "MesTMP" -v "" \
-			-i //MesTMP -t attr -n "ID" -v "$Mes" \
-			-r //MesTMP -v "Mes" Doc.xml
-		
-		[ "$(xmlstarlet sel -t -v "count(/Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia])" Doc.xml)" -eq 0 ] && xmlstarlet ed -L \
-			-s /Data/Year[@ID=$Year]/Mes[@ID=$Mes] -t elem -n "DiaTMP" -v "" \
-			-i //DiaTMP -t attr -n "ID" -v "$Dia" \
-			-r //DiaTMP -v "Dia" \
-			-s /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia] -t elem -n "MANANA" -v "" \
-			-s /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia] -t elem -n "TARDE" -v "" \
-			-s /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/* -t elem -n Hora -v "" \
-			-s /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/* -t elem -n Tardanza -v "" \
-			-s /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/* -t elem -n Excuza -v "" \
-			Doc.xml
-
+		Fecha=$(date -d"${Array[1]} ${Array[2]} today - 240 minutes" +%m/%d/%Y)		
+		. Current.ini
 		if [ $(date --date "$HoraAbrio" +%H%M) -lt 1300 ] ;then
-			
-			[ -z "$(xmlstarlet sel -t -v /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/MANANA/Hora Doc.xml)" ] || { echo "TIEMPO=0" > Data.ini && break; }
-			
-			. Current.ini
+			[ -z "$(grep "$Fecha"_1 -w < Registro)" ] || break
 			
 			if [ $(date --date "$HoraAbrio" +%H%M) -ge 0840 ] ;then
-				TEXT="LA HORA DE ABRIR LA BANCA ES A LAS 8:30 am"
-				TEXT2="Vemos pertinente recordarle que debe cumplir el horario de trabajo\n estipulado por las normas de la empresa, ya  que, de no poseer una\n justificación correcta, podría ameritar la aplicación de sanciones."
-				convert -size 1080x200 xc:powderblue -font helvetica -fill blue -gravity north -pointsize 40 -annotate +0+20 "$TEXT" \
-				-font arial -fill blue -pointsize 28 -gravity north -annotate +0+75 "$TEXT2" Imagen.jpg
-				
-				
-				Mensage=$(yad --image="Imagen.jpg"  --geometry 1080x200+0+30 --image-on-top \
-					--skip-taskbar --undecorated --on-top \
-					--form --field="Puedes escribir el motivo de la tardanza aqui.":LBL --field="" \
-					--button=gtk-ok  --buttons-layout=center )
+				TEXT="<span font='32' foreground='red' ><b>LA HORA DE ABRIR LA BANCA ES A LAS <big><big><big><sub>\
+<span bgcolor='aqua'>8:30 am</span></sub></big></big></big></b>  \n</span> \
+<span font='22' foreground='black' >Vemos pertinente recordarle que debe cumplir el horario \
+de trabajo estipulado por las normas de la empresa, ya que, de no poseer una justificación correcta, \
+podría ameritar la aplicación de sanciones.\n</span>"
+
+				convert  -font times -background white -size 1080 -define pango:justify=true pango:"$TEXT" Imagen.jpg
+							
+				Mensage=$(yad --image="Imagen.jpg"  --geometry 1080x200+$PX+30 --image-on-top \
+				--skip-taskbar --undecorated --on-top \
+				--form --field="<span font='time 15' foreground='blue'><b>Puedes escribir el motivo de la tardanza aqui.</b></span> ":LBL --field="" \
+				--button=gtk-ok  --buttons-layout=center )
 				Razon=$(echo $Mensage | awk -F "|" '{print $2}')
 				
-				xmlstarlet ed -L \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/MANANA/Hora -v "$HoraAbrio" \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/MANANA/Tardanza -v "True" \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/MANANA/Excuza -v "$Razon" \
-				Doc.xml
+				echo "$Fecha""_1=$HoraAbrio|TARDE|$Razon" >> Registro
+				echo "TIEMPO=0" > Data.ini
+				curl $URL -d ifq \
+				-d "entry.$FECHA=$(date --date $Fecha +%d/%m/%Y)" \
+				-d "entry.$BANCA=${Banca}" \
+				-d "entry.$TURNO=Entrada 1" \
+				-d "entry.$HORA=$HoraAbrio" \
+				-d "entry.$PUNTUAL=NO" \
+				-d "entry.$RAZON=$Razon"
 				
 				TEXT3="<span size='25600' rise='-20480' foreground='blue' >Te exhortamos cumplir con el horario correspondiente y evitar incurrir en faltas en futuras ocasiones.</span>"
 				convert -background linen -size 650 -define pango:justify=true pango:"$TEXT3" Imagen2.jpg
-				yad --image="Imagen2.jpg" --no-buttons --undecorated --skip-taskbar --center  --timeout=5
-				
-				echo "TIEMPO=0" > Data.ini
-				echo "Banca=${Banca} Turno=1 Hora=$HoraAbrio Tardanza=True Razon=$Razon" | mutt -s "Banca ${Banca}" -- bancalarapidamail@gmail.com
+				yad --image="Imagen2.jpg" --no-buttons --undecorated --skip-taskbar --center  --timeout=7 --on-top				
 				
 			elif [ $(date --date "$HoraAbrio" +%H%M) -lt 0840 ] ;then
-				xmlstarlet ed -L \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/MANANA/Hora -v "$HoraAbrio" \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/MANANA/Tardanza -v "False" \
-				Doc.xml
+				echo "$Fecha""_1=$HoraAbrio|OK" >> Registro	
 				echo "TIEMPO=0" > Data.ini
-				echo "Banca=${Banca} Turno=1 Hora=$HoraAbrio Tardanza=False" | mutt -s "Banca ${Banca}" -- bancalarapidamail@gmail.com
+				curl $URL -d ifq \
+				-d "entry.$FECHA=$(date --date $Fecha +%d/%m/%Y)" \
+				-d "entry.$BANCA=${Banca}" \
+				-d "entry.$TURNO=Entrada 1" \
+				-d "entry.$HORA=$HoraAbrio" \
+				-d "entry.$PUNTUAL=SI"		
 			fi					
 		
 		elif [ $(date --date "$HoraAbrio" +%H%M) -gt 1600 ] ;then
 			[ $(date +%w) -eq 0 ] && break
-			[ -z "$(xmlstarlet sel -t -v /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/TARDE/Hora Doc.xml)" ] || { echo "TIEMPO=0" > Data.ini && break; }
-		
-			. Current.ini
-		
+			[ -z "$(grep "$Fecha"_2 -w < Registro)" ] || break
+			
 			if [ $(date --date "$HoraAbrio" +%H%M) -ge 1710 ] ;then
-				TEXT="LA HORA DE ABRIR LA BANCA ES A LAS 5:00 p.m"
-				TEXT2="Vemos pertinente recordarle que debe cumplir el horario de trabajo\n estipulado por las normas de la empresa, ya  que, de no poseer una\n justificación correcta, podría ameritar la aplicación de sanciones."
-				convert -size 1080x200 xc:azure -font helvetica -fill blue -gravity north -pointsize 40 -annotate +0+20 "$TEXT" \
-				-font arial -fill blue -pointsize 28 -gravity north -annotate +0+75 "$TEXT2" Imagen.jpg
-				
-				
-				Mensage=$(yad --image="Imagen.jpg"  --geometry 1080x200+0+30 --image-on-top \
-					--skip-taskbar --undecorated --on-top \
-					--form --field="Puedes escribir el motivo de la tardanza aqui.":LBL --field="" \
-					--button=gtk-ok  --buttons-layout=center)
+				TEXT="<span font='31.5' foreground='red' ><b>LA HORA DE ABRIR LA BANCA ES A LAS <big><big><big><sub>\
+<span bgcolor='aqua'>5:00 pm</span></sub></big></big></big></b>  \n</span> \
+<span font='22' foreground='black' >Vemos pertinente recordarle que debe cumplir el horario \
+de trabajo estipulado por las normas de la empresa, ya que, de no poseer una justificación correcta, \
+podría ameritar la aplicación de sanciones.\n</span>"
+
+				convert  -font times -background white -size 1080 -define pango:justify=true pango:"$TEXT" Imagen.jpg
+							
+				Mensage=$(yad --image="Imagen.jpg"  --geometry 1080x200+$PX+30 --image-on-top \
+				--skip-taskbar --undecorated --on-top \
+				--form --field="<span font='time 15' foreground='blue'><b>Puedes escribir el motivo de la tardanza aqui.</b></span> ":LBL --field="" \
+				--button=gtk-ok  --buttons-layout=center )
 				Razon=$(echo $Mensage | awk -F "|" '{print $2}')
 	
-				xmlstarlet ed -L \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/TARDE/Hora -v "$HoraAbrio" \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/TARDE/Tardanza -v "True" \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/TARDE/Excuza -v "$Razon" \
-				Doc.xml
-				
+				echo "$Fecha""_2=$HoraAbrio|TARDE|$Razon" >> Registro
+				echo "TIEMPO=0" > Data.ini	
+				curl $URL -d ifq \
+				-d "entry.$FECHA=$(date --date $Fecha +%d/%m/%Y)" \
+				-d "entry.$BANCA=${Banca}" \
+				-d "entry.$TURNO=Entrada 2" \
+				-d "entry.$HORA=$HoraAbrio" \
+				-d "entry.$PUNTUAL=NO" \
+				-d "entry.$RAZON=$Razon"
+					
 				TEXT3="<span size='25600' rise='-20480' foreground='blue' >Te exhortamos cumplir con el horario correspondiente y evitar incurrir en faltas en futuras ocasiones.</span>"
 				convert -background linen -size 650 -define pango:justify=true pango:"$TEXT3" Imagen2.jpg
-				yad --image="Imagen2.jpg" --no-buttons --undecorated --skip-taskbar --center  --timeout=5
+				yad --image="Imagen2.jpg" --no-buttons --undecorated --skip-taskbar --center  --timeout=7 --on-top
 				
-				echo "TIEMPO=0" > Data.ini
-				echo "Banca=${Banca} Turno=2 Hora=$HoraAbrio Tardanza=True Razon=$Razon" | mutt -s "Banca ${Banca}" -- bancalarapidamail@gmail.com
 				
 			elif [ $(date --date "$HoraAbrio" +%H%M) -lt 1710 ] ;then
-				xmlstarlet ed -L \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/TARDE/Hora -v "$HoraAbrio" \
-				-u /Data/Year[@ID=$Year]/Mes[@ID=$Mes]/Dia[@ID=$Dia]/TARDE/Tardanza -v "False" \
-				Doc.xml
+	
+				echo "$Fecha""_2=$HoraAbrio|OK" >> Registro
 				echo "TIEMPO=0" > Data.ini
-				
-				echo "Banca=${Banca} Turno=2 Hora=$HoraAbrio Tardanza=False" | mutt -s "Banca ${Banca}" -- bancalarapidamail@gmail.com
+				curl $URL -d ifq \
+				-d "entry.$FECHA=$(date --date $Fecha +%d/%m/%Y)" \
+				-d "entry.$BANCA=${Banca}" \
+				-d "entry.$TURNO=Entrada 2" \
+				-d "entry.$HORA=$HoraAbrio" \
+				-d "entry.$PUNTUAL=SI"			
+							
 			fi	
 			
 		fi
 	
-    break
+		break
 	fi
 echo "TIEMPO=$((ACUMULADO+$(</proc/uptime awk '{printf int ($1)}')))" > Data.ini
 sleep 2
 done
 exit
-
