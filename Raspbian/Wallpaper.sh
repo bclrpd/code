c05a494e6a54af9a68d1709fa679b8e8e15742a6
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-04-08 19:52:09 -0400

    Create Wallpaper.sh

---
+++ Raspbian/Wallpaper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+until ping -nq -c3 8.8.8.8; do
+		sleep 1
+done
+wget https://raw.githubusercontent.com/bclrpd/code/main/Wallpaper.jpg -O Wallpaper.jpg
+[ $? -eq 0 ] && pcmanfm --set-wallpaper "/home/ventas/.Auto/Wallpaper.jpg"
+rm Wallpaper.sh
+exit
