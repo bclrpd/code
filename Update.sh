a770ece8e18eb5b0bdc6982ce7d6faaff49e26db
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-04-08 20:28:59 -0400

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -18,6 +18,7 @@ do
 		[ $? -eq 0 ] || X=1 
 	fi
 done
-bash Wallpaper.sh &
 [ $X -eq 0 ] && echo "Version=$1" > Current.ini && echo "Banca=$2" >> Current.ini && rm Update.sh
+sleep 20
+bash Wallpaper.sh &
 exit
