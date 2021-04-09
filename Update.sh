221fb9e20bf9854c71f03d7fda46b4d1e3699b93
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-04-09 00:07:59 -0400

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -22,6 +22,5 @@ done
 
 wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/Wallpaper.jpg -O Wallpaper.jpg
 [ $? -eq 0 ] && pcmanfm --set-wallpaper "/home/ventas/.Auto/Wallpaper.jpg"
-rm Wallpaper.sh
 
 exit
