7c28c5cc500853609c545b969e191aedf436990b
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-04-08 20:06:06 -0400

    Update Wallpaper.sh

--- Raspbian/Wallpaper.sh
+++ Raspbian/Wallpaper.sh
@@ -2,7 +2,7 @@
 until ping -nq -c3 8.8.8.8; do
 		sleep 1
 done
-wget https://raw.githubusercontent.com/bclrpd/code/main/Wallpaper.jpg -O Wallpaper.jpg
+wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/Wallpaper.jpg -O Wallpaper.jpg
 [ $? -eq 0 ] && pcmanfm --set-wallpaper "/home/ventas/.Auto/Wallpaper.jpg"
 rm Wallpaper.sh
 exit
