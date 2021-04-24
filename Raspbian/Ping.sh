ec33d4016e4002aa75234b3253b516bbe110d78c
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-04-23 20:17:02 -0400

    Update Ping.sh

--- Raspbian/Ping.sh
+++ Raspbian/Ping.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+#eliminar esta linea
+sed -i 's/.*desktop_fg=#.*/desktop_fg=#f684097b097b/' ~/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+#eliminar
+
+
 cd "$(dirname "$0")"
 xmodmap -e "pointer = 1 2 0"
 bash Inicio.sh &
