2030a6ffa5b8e9167633aa82150702355feb68cc
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-05-26 15:04:31 -0400

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -2,9 +2,9 @@
 cd "$(dirname "$0")"
 URL=https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/
 if [ "$3" == "A" ] ; then
-	Archivo=(ControlHorario.sh Apagado.sh CloneMac.sh Impresora.sh Inicio.sh Ping.sh ShutdownButton.sh UpdateChek.sh Reboot.py chromium-browser)
+	Archivo=(ControlHorario.sh Apagado.sh CloneMac.sh Impresora.sh Inicio.sh Ping.sh ShutdownButton.sh UpdateChek.sh Reboot.py chromium-browser Descargar.sh)
 else
-	Archivo=(ControlHorarioB.sh Apagado.sh CloneMac.sh Impresora.sh Inicio.sh PingB.sh ShutdownButton.sh UpdateChek.sh Reboot.py)
+	Archivo=(ControlHorarioB.sh Apagado.sh CloneMac.sh Impresora.sh Inicio.sh PingB.sh ShutdownButton.sh UpdateChek.sh Reboot.py chromium-browser Descargar.sh)
 fi
 
 X=0
@@ -24,17 +24,5 @@ done
 wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/LogoPrinter.png -O /home/ventas/lotobet/.61606.png
 wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/Wallpaper.jpg -O Wallpaper.jpg
 [ $? -eq 0 ] && pcmanfm --set-wallpaper "/home/ventas/.Auto/Wallpaper.jpg"
-
-exit
-
-#--------Descargar chromedriver
-if [ "$(du /home/ventas/.Auto/chromedriver -b | awk 'NR==1 {print $1}')" == "11431612" ] ;then
-	echo "Ok"
-else
-	wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/chromedriver -O-
-fi
-
-#-----Instalar selenium
-pip3 install selenium
 
 exit
