192f20a5abb7b4241d425a42f12a8bea1dd345ac
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-05-29 18:30:19 -0400

    Update Apagado.sh

--- Raspbian/Apagado.sh
+++ Raspbian/Apagado.sh
@@ -1,16 +1,28 @@
 #! /bin/bash
-while true ; do
-	sleep 10
-	until ping -nq -c3 8.8.8.8; do
-		sleep 1
-	done
-	
-	sleep 300
-	
-	if [ $(date +%w) -ne 0 ] ; then
-		shutdown 15:10
-		break
-	else
+until ping -nq -c3 8.8.8.8; do
+	sleep 1
+done
+sleep 300
+
+. Current.ini
+
+if [ "${Tipo}" = "B" ] ;then
+	while true ; do
+	sleep 20	
+	if [ $(date +%w) -ne 0 ] && [ $(date +%H%M) -eq 2110 ] ; then
+		shutdown -h now
 		break
 	fi	
-done
+	done
+else
+	while true ; do
+	sleep 20	
+	if [ $(date +%w) -ne 0 ] && [ $(date +%H%M) -eq 1510 ] ; then
+		shutdown -h now
+		break
+	elif [ $(date +%w) -ne 0 ] && [ $(date +%H%M) -eq 2110 ] ; then
+		shutdown -h now
+		break
+	fi	
+	done
+fi
