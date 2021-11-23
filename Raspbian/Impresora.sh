4ac6638f11f3f4b85b37c612c0201880b8faa260
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-11-23 11:46:18 -0400

    Update Impresora.sh

--- Raspbian/Impresora.sh
+++ Raspbian/Impresora.sh
@@ -18,3 +18,23 @@ if [ ! -z $NEWURI ] ; then
 fi
 sleep 2
 done
+
+#--------Sincronizar Hora-------------
+until ping -nq -c3 8.8.8.8; do
+	sleep 1
+done
+sleep 5
+while [ -z "$TIME" ]; do
+	TIME="$(curl -s --head http://google.com | grep ^Date: | sed 's/Date: //g')"
+	sleep 1
+done
+
+if [ "$(timedatectl | grep "System clock synchronized:" | awk '{print $4}')" != "yes" ] ; then
+	timedatectl set-ntp false
+	sleep 1
+	TIME2=$(date -d "$TIME" +"%Y-%m-%d %H:%M:%S")
+	timedatectl set-time "$TIME2"
+	sleep 1
+	timedatectl set-ntp true
+	echo "hecho"
+fi
