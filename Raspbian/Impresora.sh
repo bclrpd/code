b23ba85d622332d192e5041340ebb282a2fd88fd
bclrpd <78045587+bclrpd@users.noreply.github.com>
2021-12-07 23:21:00 -0400

    Update Impresora.sh

--- Raspbian/Impresora.sh
+++ Raspbian/Impresora.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+cd "$(dirname "$0")" 				#Esta linea desaparece con la primera ejecucion
 lpadmin -p Impresora -v "serial:/dev/ttyUSB0" 	#Esta linea desaparece con la primera ejecucion
-sed -i '2,3d' ./home/ventas/.Auto/Impresora.sh 	#Esta linea desaparece con la primera ejecucion
+sed -i '2,4d' ./Impresora.sh 			#Esta linea desaparece con la primera ejecucion
 
 #-----Agregar Vendor de la Impresora a /usr/share/cups/usb/org.cups.usb-quirks
 Add_Vendor_Printer () {
