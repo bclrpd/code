e16df238e8d2eab32fd600867735e98923b4857f
bclrpd <78045587+bclrpd@users.noreply.github.com>
2022-11-01 14:42:50 -0400

    Update Imprimir.sh

--- Raspbian/Imprimir.sh
+++ Raspbian/Imprimir.sh
@@ -10,7 +10,7 @@ echo "  " > Premios.ini
 wget https://drive.google.com/u/0/uc?id=1xoK8xsJ014ijRmCHnP4ZNjyv_NTI7thh -O Premios.ini
 [ $? -eq 0 ] || exit 0
 Premios="$( awk '/\[/{prefix=$0; next} $1{print prefix $0}' Premios.ini)"
-Loterias=("La Primera 12pm" "La Primera 8pm" "Q Real" "Nac T" "Nac N" "Suerte 12.30pm" "Suerte 6pm" "Loteka" "Q Pale" "Lotedom" "Ny Dia" "Ny Noche" "FL Tarde" "FL Noche" "Anguilla 10am" "Anguilla 1pm" "Anguilla 6pm" "Anguilla 9pm")
+Loterias=("Primera 12pm" "Primera 8pm" "Q Real" "Nac T" "Nac N"  "Loteka" "Q Pale" "Lotedom" "Suerte 12.30pm" "Suerte 6pm" "Ny Dia" "Ny Noche" "FL Tarde" "FL Noche" "Anguilla 10am" "Anguilla 1pm" "Anguilla 6pm" "Anguilla 9pm")
 
 if [ $(date +%H%M) -lt 1200 ] ; then 
 	Fecha=$(date --date="-1 day" +%d-%m-%Y)
