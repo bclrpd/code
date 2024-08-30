7225e3f675f247cc9be559100d814484d3bda78d
bclrpd <78045587+bclrpd@users.noreply.github.com>
2024-08-30 19:51:42 -0400

    Update Boton_Premios.sh

--- Raspbian/Boton_Premios.sh
+++ Raspbian/Boton_Premios.sh
@@ -4,7 +4,9 @@ until ping -nq -c3 8.8.8.8; do
 	sleep 1
 done
 
-while true; do
-Imprimir=$(yad --center --skip-taskbar --on-top --undecorated --geometry=+740+55 --button="Premios")
-bash Imprimir.sh
-done
+yad --text="Premios" --text-align=center --form --columns=2 \
+--center --skip-taskbar --on-top --undecorated --geometry=+720+38 --no-buttons \
+--field="Ayer":fbtn "bash Imprimir.sh Ayer" \
+--field="Hoy":fbtn "bash Imprimir.sh Hoy"
+
+exit
