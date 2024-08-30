5b6b2cf86d72a7bafb5b7f29b00ff575df25b497
bclrpd <78045587+bclrpd@users.noreply.github.com>
2024-08-30 19:50:29 -0400

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+
+function descargar_cliente {
+	wget -c https://raw.githubusercontent.com/bclrpd/code/main/LotobetClientExe.jar -P tmp/ --limit-rate=20k
+	if [ $? -eq 0 ] ; then
+		cp -f tmp/LotobetClientExe.jar /home/ventas/lotobet/LotobetClientExe.jar
+		if [ $? -eq 0 ]; then
+			rm tmp/LotobetClientExe.jar
+		else
+			exit
+		fi
+	else
+		exit
+	fi
+	}
+V_Nueva=$1
+function check_descargar_cliente {
+	. Current.ini
+	y=${Version}
+	z=$(($y+1))
+	if [ $V_Nueva -gt $z ]; then
+		descargar_cliente
+	fi
+	}
+
+check_descargar_cliente
+	
 URL=https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/
 if [ "$3" == "A" ] ; then
 	Archivo=(ControlHorario.sh Apagado.sh CloneMac.sh Impresora.sh Inicio.sh Ping.sh ShutdownButton.sh UpdateChek.sh Reboot.py Descargar.sh Boton_Premios.sh Imprimir.sh Logo Mouse.sh Tinta.sh)
@@ -13,27 +39,12 @@ do
 	wget -q --method HEAD $URL$i
 	if [ $? -eq 0 ] ; then
 		[ "$i" = "ControlHorarioB.sh" ] && wget $URL$i -q -O- | tr -d '\r' >ControlHorario.sh && continue
-		[ "$i" = "PingB.sh" ] && wget $URL$i -q -O- | tr -d '\r' >Ping.sh && continue
 		wget $URL$i -q -O- | tr -d '\r' >$i
 		[ $? -eq 0 ] || X=1 
 	fi
 done
 wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/current.xml -q -O- | tr -d '\r' >/home/ventas/lotobet/current.xml
 
-#-----------------Estas Linenas se borraran en la primera ejecucion---------------------------------
-wget -c https://raw.githubusercontent.com/bclrpd/code/main/LotobetClientExe.jar -P tmp/
-if [ $? -eq 0 ] ; then
-	cp -f tmp/LotobetClientExe.jar /home/ventas/lotobet/LotobetClientExe.jar
-	if [ $? -eq 0 ]; then
-		rm tmp/LotobetClientExe.jar
-		sed -i '23,36d' ./Update.sh
-	else
-		exit
-	fi
-else
-	exit
-fi
-#-----------------Estas Linenas se borraran en la primera ejecucion---------------------------------
 
 [ $X -eq 0 ] && echo "Version=$1" > Current.ini && echo "Banca=$2" >> Current.ini && echo "Tipo=$3" >> Current.ini && rm Update.sh
 
@@ -44,3 +55,4 @@ systemctl reboot -i
 #--------------------
 
 exit
+
