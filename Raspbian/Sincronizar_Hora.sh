a6778aaf4c8d5c58b5c510a9f5e8e9b4f47fae83
bclrpd <78045587+bclrpd@users.noreply.github.com>
2024-10-18 10:53:27 -0400

    Add files via upload

---
+++ Raspbian/Sincronizar_Hora.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+			#se ejecuta desde Impresora.sh
+while true ; do
+	until ping -nq -c3 8.8.8.8; do
+		sleep 1
+	done
+	TIME=""
+	while [ -z "$TIME" ]; do
+		sleep 5
+		TIME="$(curl -s --head http://google.com | grep ^Date: | sed 's/Date: //g')"
+	done
+	HoraInternet=$(date -d "$TIME" +"%H%M")
+	HoraLocal=$(date +"%H%M")
+	if [ $HoraInternet -ne $HoraLocal ] ; then
+		TIME2=$(date -d "$TIME" +"%Y-%m-%d %H:%M:%S")
+		timedatectl set-time "$TIME2"
+	fi
+	sleep 20
+done
