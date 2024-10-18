c1b29bab2587a43429a68624fc91c41375e8c765
bclrpd <78045587+bclrpd@users.noreply.github.com>
2024-10-18 12:32:27 -0400

    Update Sincronizar_Hora.sh

--- Raspbian/Sincronizar_Hora.sh
+++ Raspbian/Sincronizar_Hora.sh
@@ -13,7 +13,11 @@ while true ; do
 	HoraLocal=$(date +"%H%M")
 	if [ $HoraInternet -ne $HoraLocal ] ; then
 		TIME2=$(date -d "$TIME" +"%Y-%m-%d %H:%M:%S")
+  		timedatectl set-ntp false
+		sleep 1
 		timedatectl set-time "$TIME2"
+  		sleep 1
+		timedatectl set-ntp true
 	fi
 	sleep 300
 done
