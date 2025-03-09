e391a9b7aaa344814786b698970bd0c65bd301a5
bclrpd <78045587+bclrpd@users.noreply.github.com>
2025-03-09 13:13:41 -0400

    Update Impresora.sh

--- Raspbian/Impresora.sh
+++ Raspbian/Impresora.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 cd "$(dirname "$0")" 				
 lpadmin -p Impresora -v "serial:/dev/ttyUSB0" 	#Esta linea desaparece con la primera ejecucion
-sed -i '302s/.*/0x0FE6 unidir soft-reset/' /usr/share/cups/usb/org.cups.usb-quirks
-sed -i '303s/.*/0x4B43 unidir/' /usr/share/cups/usb/org.cups.usb-quirks
-sed -i '304s/.*/0x2ca6 0x811a unidir/' /usr/share/cups/usb/org.cups.usb-quirks
-#sed -i '305d' /usr/share/cups/usb/org.cups.usb-quirks
-sed -i '3,9d' ./Impresora.sh 			#Esta linea desaparece con la primera ejecucion
+vendor=('0x0FE6 unidir soft-reset' '0x4B43 unidir' '0x2ca6 0x811a unidir')
+for i in "${vendor[@]}" ; do
+	existe=$(grep "$i" /usr/share/cups/usb/org.cups.usb-quirks)
+	if [ -z "$existe" ] ; then
+		echo "$i" >> /usr/share/cups/usb/org.cups.usb-quirks
+	fi	
+done
+sed -i '3,12d' ./Impresora.sh 			#Esta linea desaparece con la primera ejecucion
 chmod 777 /dev/vchiq
 bash Sincronizar_Hora.sh &  #Script para mantener hora sincronizada
 while true ; do
