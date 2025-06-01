fd9f6f6da6d3ea244fc740e0ee6627f87f3e3d7d
bclrpd <78045587+bclrpd@users.noreply.github.com>
2025-06-01 17:25:44 -0400

    Update Impresora.sh

--- Raspbian/Impresora.sh
+++ Raspbian/Impresora.sh
@@ -10,7 +10,8 @@ for i in "${vendor[@]}" ; do
 		echo "$i" >> /usr/share/cups/usb/org.cups.usb-quirks
 	fi	
 done
-sed -i '3,13d' ./Impresora.sh 			#Esta linea desaparece con la primera ejecucion
+sudo apt-get remove network-manager-gnome
+sed -i '3,14d' ./Impresora.sh 			#Esta linea desaparece con la primera ejecucion
 bash Sincronizar_Hora.sh &  #Script para mantener hora sincronizada
 while true ; do
 
