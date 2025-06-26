9c0233e474e06d3a967b29e9f7b72736ffa98ffe
bclrpd <78045587+bclrpd@users.noreply.github.com>
2025-06-26 17:12:49 -0400

    Update Udt.sh

--- Udt.sh
+++ Udt.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 
+wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/CanalWifi.py -O CanalWifi.py 
+if [ $? -eq 0 ] ; then
+    if ping 192.168.1.1 -i 0.5 -w 5 ; then
+        python3 /home/ventas/.Auto/CanalWifi.py Alcatel &		
+    elif ping 192.168.8.1 -i 0.5 -w 5 ; then
+        python3 /home/ventas/.Auto/CanalWifi.py Huawei &
+    fi    
+else;
+    exit
+fi
+sleep 30
+
+until ping -nq -c3 8.8.8.8; do
+	sleep 1
+done
+
 inf=986953837
 URL="https://docs.google.com/forms/d/e/1FAIpQLSd0pRPkVt8oigNw-CZVuMyESFybTi1Me1ORbENgJWiuaothwg/formResponse"
 . Current.ini
@@ -10,9 +26,9 @@ Modelo=$(cat /sys/firmware/devicetree/base/model)
 ip=$(ip route | grep default | awk '{print $3}')
 interface=$(ip route | grep default | awk '{print $5}')
 osversion=$(cat /etc/debian_version)
-
+canal=$(iw wlan0 info | grep channel | awk '{print $2}')
 curl $URL -d ifq \
-	-d "entry.$inf=$banca|$Modelo|$osversion|$memoria|$interface|$ip" 
+	-d "entry.$inf=$banca|$Modelo|$osversion|$memoria|$interface|$ip|$canal" 
 
 echo "Version=$1" > Current.ini && echo "Banca=$2" >> Current.ini && echo "Tipo=$3" >> Current.ini && rm Update.sh
 
