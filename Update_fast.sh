01a17797dcd8663ebdf8c7aa7a6b962e32fb9b4c
bclrpd <78045587+bclrpd@users.noreply.github.com>
2025-06-27 17:20:02 -0400

    Update Update_fast.sh

--- Update_fast.sh
+++ Update_fast.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/CanalWifi.py -O CanalWifi.py 
 if [ $? -eq 0 ] ; then
     if ping 192.168.1.1 -i 0.5 -w 5 ; then
-        python3 /home/ventas/.Auto/CanalWifi.py Alcatel 		
+        lxterminal -e "python3 /home/ventas/.Auto/CanalWifi.py Alcatel" 		
     elif ping 192.168.8.1 -i 0.5 -w 5 ; then
         python3 /home/ventas/.Auto/CanalWifi.py Huawei 
     fi    
