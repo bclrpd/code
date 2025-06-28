304164eba7bb8391248251a181ec0e5701fbc824
bclrpd <78045587+bclrpd@users.noreply.github.com>
2025-06-28 14:07:18 -0400

    Update Update_fast.sh

--- Update_fast.sh
+++ Update_fast.sh
@@ -1,29 +1,21 @@
 #! /bin/bash
 cd "$(dirname "$0")"
-echo "1" >> aa
 wget https://raw.githubusercontent.com/bclrpd/code/main/Raspbian/CanalWifi.py -O CanalWifi.py 
 if [ $? -eq 0 ] ; then
-echo "2" >> aa
 	ip=$(ip route | grep default | awk '{print $3}')
     if [ $ip = "192.168.1.1" ] ; then
-    	echo "2.1" >> aa
         python3 /home/ventas/.Auto/CanalWifi.py Alcatel &
-	echo "2.2" >> aa
     elif [ $ip = "192.168.8.1" ] ; then
         python3 /home/ventas/.Auto/CanalWifi.py Huawei &
     fi    
 else
     exit
-    echo "3" >> aa
 fi
-echo "4" >> aa
 sleep 30
 while [ -z "$TIME" ]; do
 	TIME="$(curl -s --head http://google.com | grep ^Date: | sed 's/Date: //g')"
- 	echo "..." >> aa
 	sleep 1
 done
-echo "5" >> aa
 inf=986953837
 URL="https://docs.google.com/forms/d/e/1FAIpQLSd0pRPkVt8oigNw-CZVuMyESFybTi1Me1ORbENgJWiuaothwg/formResponse"
 . Current.ini
@@ -37,7 +29,6 @@ canal=$(iw wlan0 info | grep channel | awk '{print $2}')
 curl $URL -d ifq \
 	-d "entry.$inf=$banca|$Modelo|$osversion|$memoria|$interface|$ip|$canal" 
 
-echo "6" >> aa
-echo "Version=$1" > Current.ini && echo "Banca=$2" >> Current.ini && echo "Tipo=$3" >> Current.ini && rm Update.sh
+echo "Version=$1" > Current.ini && echo "Banca=$2" >> Current.ini && echo "Tipo=$3" >> Current.ini && rm Update.sh && rm CanalWifi.py
 
 exit
