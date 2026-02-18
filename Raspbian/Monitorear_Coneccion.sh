6cfe9c77aebad8c8f1a230d87bcd09cef6dbf293
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-02-18 11:07:16 -0400

    Add files via upload

---
+++ Raspbian/Monitorear_Coneccion.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+contador=1
+while true ; do
+    idle_time=$(xprintidle)
+    if [ "$idle_time" -lt 60000 ] ; then
+        contador=1
+        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q)
+        lista=( $ping_result )
+        if  [ ${#lista[@]} -gt 16 ] ; then
+            echo "$(date +%s)/${lista[17]//%/}/${lista[25]}" >> Registro_ping
+        fi
+    elif [ "$idle_time" -gt $((contador * 30000)) ] ; then
+        ((contador++))
+        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q)
+        lista=( $ping_result ) 
+        if  [ ${#lista[@]} -gt 16 ] ; then
+            echo "$(date +%s)/${lista[17]//%/}/${lista[25]}" >> Registro_ping
+        fi
+    else
+        sleep 1
+    fi
+done
---
+++ Raspbian/Subir_archivo.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def get_url(nombre_archivo):
+    # URL que te proporcionó AWS (Function URL o API Gateway)
+    url = "https://vpjt2hz4h4uqnqblkewmpugi4m0hqyyn.lambda-url.us-east-1.on.aws/"
+    data = {"nombre": nombre_archivo}
+    response = requests.post(url, json=data, timeout=10)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"Error {response.status_code}: {response.text}"
+
+def subir_archivo_con_url(ruta_archivo_local, url_firmada):
+    with open(ruta_archivo_local, 'rb') as f:
+        # Es crucial que el método sea PUT si se generó para 'put_object'
+        response = requests.put(url_firmada, data=f, timeout=100)
+    
+    if response.status_code == 200:
+        print("Carga exitosa a S3")
+    else:
+        print(f"Error en la carga: {response.status_code} - {response.text}")
+
+def get_banca():
+    with open("Current.ini", "r", encoding="utf-8") as f:
+        for linea in f:
+            if "Banca" in linea:
+                linea = linea.strip()
+                return f'{linea.split("=")[1]}.txt'
+
+banca = get_banca()
+url = get_url(banca)  
+subir_archivo_con_url('Registro_ping', url)
