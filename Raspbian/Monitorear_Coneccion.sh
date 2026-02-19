e8546cffec1bf35e3e22ef9918340de71d6f5a56
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-02-18 20:46:33 -0400

    Update Monitorear_Coneccion.sh

--- Raspbian/Monitorear_Coneccion.sh
+++ Raspbian/Monitorear_Coneccion.sh
@@ -7,15 +7,19 @@ while true ; do
         contador=1
         ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q)
         lista=( $ping_result )
-        if  [ ${#lista[@]} -gt 16 ] ; then
+        if  [ ${#lista[@]} -eq 27 ] ; then
             echo "$(date +%FT%T)/${lista[17]//%/}/${lista[25]}" >> Registro_ping
+        else
+            echo "$(date +%FT%T)/$ping_result" tr -d '\n' >> Registro_ping
         fi
     elif [ "$idle_time" -gt $((contador * 30000)) ] ; then
         ((contador++))
         ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q)
         lista=( $ping_result ) 
-        if  [ ${#lista[@]} -gt 16 ] ; then
+        if  [ ${#lista[@]} -eq 27 ] ; then
             echo "$(date +%FT%T)/${lista[17]//%/}/${lista[25]}" >> Registro_ping
+         else
+            echo "$(date +%FT%T)/$ping_result" tr -d '\n' >> Registro_ping
         fi
     else
         sleep 1
