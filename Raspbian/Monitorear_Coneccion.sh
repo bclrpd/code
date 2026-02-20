2921f8e460d79d114d6cbc27d62ff9b63870c116
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-02-20 15:42:03 -0400

    Refactor ping command and adjust sleep duration

--- Raspbian/Monitorear_Coneccion.sh
+++ Raspbian/Monitorear_Coneccion.sh
@@ -3,52 +3,43 @@ cd "$(dirname "$0")"
 contador=1
 while true ; do
     idle_time=$(xprintidle)
+    estadistic=""
+    pqtPerdidos=""
     if [ "$idle_time" -lt 60000 ] ; then
         contador=1
-        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q)
+        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q 2>&1)
         if [[ "$ping_result" == *"min/avg/max/mdev"* ]]; then
             estadistica=$(echo ${ping_result##*'mdev = '} | awk '{ print $1}')
+        fi
+        if [[ "$ping_result" == *"packet loss"* ]]; then 
             pqtPerdidos=$(echo ${ping_result%%'packet loss'*} | awk '{ print $NF}')
-        elif [[ "$ping_result" == *"packet loss"* ]]; then 
-            estadistica=""
-            pqtPerdidos=$(echo ${ping_result%%'packet loss'*} | awk '{ print $NF}')
-        else 
-            estadistica=""
-            pqtPerdidos=""
         fi
         
-        if [ ! -z "$pqtPerdidos" ] ; then
-            echo "$(date +%FT%T)/$pqtPerdidos/$estadistica" >> Registro_ping
-        else
+        if [ -z "$pqtPerdidos" ] ; then
             echo "$(date +%FT%T)/$ping_result" | tr -d '\n' >> Registro_ping
             echo '' >> Registro_ping
-            sleep 5
+            sleep 10
+        else
+            echo "$(date +%FT%T)/$pqtPerdidos/$estadistica" >> Registro_ping
         fi
-        
-        echo $estadistica
-        echo $pqtPerdidos
+
     elif [ "$idle_time" -gt $((contador * 30000)) ] ; then
         ((contador++))
-        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q)
+        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q 2>&1)
         if [[ "$ping_result" == *"min/avg/max/mdev"* ]]; then
             estadistica=$(echo ${ping_result##*'mdev = '} | awk '{ print $1}')
+        fi
+        if [[ "$ping_result" == *"packet loss"* ]]; then 
             pqtPerdidos=$(echo ${ping_result%%'packet loss'*} | awk '{ print $NF}')
-        elif [[ "$ping_result" == *"packet loss"* ]]; then 
-            estadistica=""
-            pqtPerdidos=$(echo ${ping_result%%'packet loss'*} | awk '{ print $NF}')
-        else 
-            estadistica=""
-            pqtPerdidos=""
         fi
         
-        if [ ! -z "$pqtPerdidos" ] ; then
-            echo "$(date +%FT%T)/$pqtPerdidos/$estadistica" >> Registro_ping
-        else
+        if [ -z "$pqtPerdidos" ] ; then
             echo "$(date +%FT%T)/$ping_result" | tr -d '\n' >> Registro_ping
             echo '' >> Registro_ping
-            sleep 5
-        fi
-    else
-        sleep 1
+            sleep 10
+        else
+            echo "$(date +%FT%T)/$pqtPerdidos/$estadistica" >> Registro_ping
+        fi  
     fi
+    sleep 1
 done
