3d58dc1526f925181d252fe8cb4a2441fcd83131
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-03-08 16:50:59 -0400

    Refactor connection monitoring script for efficiency

--- Raspbian/Monitorear_Coneccion.sh
+++ Raspbian/Monitorear_Coneccion.sh
@@ -1,45 +1,48 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 contador=1
-while true ; do
-    idle_time=$(xprintidle)
+
+Proceder(){
     estadistic=""
     pqtPerdidos=""
+    modem="$(cat info.ini | grep 'Modem=' | cut -d'=' -f2)"
+    red="$(cat info.ini | grep 'Red_Conectada=' | cut -d'=' -f2)"
+    interface="$(cat info.ini | grep 'Intercafe=' | cut -d'=' -f2)"
+    wifiSignal="$(cat info.ini | grep 'wifi_Signal=' | cut -d'=' -f2)"
+    wifiQuality="$(cat info.ini | grep 'wifi_Quality=' | cut -d'=' -f2)"
+    Nivel_Senal="$(cat info.ini | grep 'Nivel_Senal=' | cut -d'=' -f2)"
+    Senal_Ruido="$(cat info.ini | grep 'Senal_Ruido=' | cut -d'=' -f2)"
+    
+    ping_result=$(ping 8.8.8.8 -i 0.2 -w 9 -q 2>&1)
+    if [[ "$ping_result" == *"min/avg/max/mdev"* ]]; then
+        estadistica=$(echo ${ping_result##*'mdev = '} | awk '{ print $1}')
+    fi
+    if [[ "$ping_result" == *"packet loss"* ]]; then 
+        pqtPerdidos=$(echo ${ping_result%%'packet loss'*} | awk '{ print $NF}' | cut -d% -f1 )
+        pqtPerdidos="$(printf '%.0f' $pqtPerdidos)"
+    fi
+    
+    if [ -z "$pqtPerdidos" ] ; then
+        echo "$(date +%FT%T)|$ping_result" | tr -d '\n' >> Registro_ping
+        echo '' >> Registro_ping
+        sleep 9
+    else
+        [[ "$pqtPerdidos" == "100" ]] && estadistica=''
+        echo "$(date +%FT%T)|$modem|$red|$interface|$wifiSignal|$wifiQuality|$Nivel_Senal|$Senal_Ruido|$pqtPerdidos|$estadistica" >> Registro_ping
+    fi
+    }
+
+while true ; do
+    idle_time=$(xprintidle)
+  
     if [ "$idle_time" -lt 60000 ] ; then
         contador=1
-        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q 2>&1)
-        if [[ "$ping_result" == *"min/avg/max/mdev"* ]]; then
-            estadistica=$(echo ${ping_result##*'mdev = '} | awk '{ print $1}')
-        fi
-        if [[ "$ping_result" == *"packet loss"* ]]; then 
-            pqtPerdidos=$(echo ${ping_result%%'packet loss'*} | awk '{ print $NF}')
-        fi
-        
-        if [ -z "$pqtPerdidos" ] ; then
-            echo "$(date +%FT%T)/$ping_result" | tr -d '\n' >> Registro_ping
-            echo '' >> Registro_ping
-            sleep 10
-        else
-            echo "$(date +%FT%T)/$pqtPerdidos/$estadistica" >> Registro_ping
-        fi
+        Proceder
 
     elif [ "$idle_time" -gt $((contador * 30000)) ] ; then
         ((contador++))
-        ping_result=$(ping 8.8.8.8 -i 0.2 -w 10 -q 2>&1)
-        if [[ "$ping_result" == *"min/avg/max/mdev"* ]]; then
-            estadistica=$(echo ${ping_result##*'mdev = '} | awk '{ print $1}')
-        fi
-        if [[ "$ping_result" == *"packet loss"* ]]; then 
-            pqtPerdidos=$(echo ${ping_result%%'packet loss'*} | awk '{ print $NF}')
-        fi
-        
-        if [ -z "$pqtPerdidos" ] ; then
-            echo "$(date +%FT%T)/$ping_result" | tr -d '\n' >> Registro_ping
-            echo '' >> Registro_ping
-            sleep 10
-        else
-            echo "$(date +%FT%T)/$pqtPerdidos/$estadistica" >> Registro_ping
-        fi  
+        Proceder
     fi
     sleep 1
 done
+
