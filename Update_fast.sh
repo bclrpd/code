8708f531c8aaded599fc22503c5f05a3c050d6f6
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-03-15 15:45:13 -0400

    Update Update_fast.sh

--- Update_fast.sh
+++ Update_fast.sh
@@ -42,7 +42,7 @@ done
 
 for i in "${Archivo[@]}"; do
     [ "$i" == "ControlHorarioB.sh" ] && i="ControlHorario.sh"
-    if [ $(stat -c%s tmp/$i) -gt 100 ] ; then
+    if grep -q "1e9e544039e5b1" tmp/$i; then
         cp -f tmp/$i /home/ventas/.Auto/$i
         if [ $? -eq 0 ]; then
 			rm tmp/$i
