ecc750a5322ac4edd3a865e304bb9f5c6385b9f4
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-05-12 19:25:38 -0400

    Add UUID generation and update script comments

--- Raspbian/Crear_Perfil2.sh
+++ Raspbian/Crear_Perfil2.sh
@@ -5,6 +5,7 @@ ORIGEN="/etc/NetworkManager/system-connections/Conexión inalámbrica 1.nmconnec
 DESTINO="/etc/NetworkManager/system-connections/Conexión inalámbrica 2.nmconnection"
 NUEVO_SSID="Cargando.."
 NUEVA_CLAVE=$(echo "VCVFVkhHTWJCZlY4ejJAaA==" | base64 --decode)
+NUEVO_UUID=$(uuid)
 
 # 1. Copiar el archivo original con permisos de root
 sudo cp "$ORIGEN" "$DESTINO"
@@ -18,10 +19,13 @@ sudo sed -i "s/^ssid=.*/ssid=$NUEVO_SSID/" "$DESTINO"
 # 4. Modificar la contraseña (PSK)
 sudo sed -i "s/^psk=.*/psk=$NUEVA_CLAVE/" "$DESTINO"
 
-# 5. Ajustar permisos (Crucial: NetworkManager ignora archivos si no son 600)
+# 5. Reemplazar el UUID viejo por el nuevo en el archivo de destino
+sudo sed -i "s/^uuid=.*/uuid=$NUEVO_UUID/" "$DESTINO"
+
+# 6. Ajustar permisos (Crucial: NetworkManager ignora archivos si no son 600)
 sudo chmod 600 "$DESTINO"
 
-# 6. Avisar a NetworkManager que hay un archivo nuevo
+# 7. Avisar a NetworkManager que hay un archivo nuevo
 sudo nmcli connection reload
 rm Crear_Perfil2.sh # elimina el script al finalizar
 echo "✅ Perfil copiado y modificado en $DESTINO"
