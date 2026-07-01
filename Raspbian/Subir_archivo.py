e696dbe32fe425d59f42ebe5d3189e4863c3844d
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-07-01 16:22:46 -0400

    Update Subir_archivo.py

--- Raspbian/Subir_archivo.py
+++ Raspbian/Subir_archivo.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import os
+from datetime import datetime, timedelta
+from pathlib import Path
 
 def get_url(nombre_archivo):
     # URL que te proporcionó AWS (Function URL o API Gateway)
@@ -30,10 +32,71 @@ def get_banca():
                 return f'{linea.split("=")[1]}.txt'
 
 
+def guardar_informaion_banca():
+    """ #organizacion de los datos a enviar
+        ( 
+            banca,
+            fecha,
+            version_Raspberry,
+            MAC,
+            interfaz_De_Red,
+            router,
+            IMEI_Router,
+            s_Version_Router,
+            telefonica,
+            numero_telefono,
+            codigo_SIM,
+        )
+    """
+    inf = {}
+    inf['Fecha'] = datetime.now().strftime("%Y%m%d%H%M%S")
+    with open("Current.ini", "r", encoding="utf-8") as f:
+        for linea in f:
+            if "Banca" in linea:
+                linea = linea.strip()
+                inf[str(linea.split("=")[0])] = linea.split("=")[1]
+                             
+    with open("info.ini", "r", encoding="utf-8") as f:
+        for linea in f:
+            if "=" in linea:
+                linea = linea.strip()
+                inf[str(linea.split("=")[0])] = linea.split("=")[1]
+                  
+    try:       
+        inf['version_pi'] = Path("/proc/device-tree/model").read_text().strip("\x00")
+        inf['MAC'] = Path("/sys/class/net/eth0/address").read_text().strip()
+    except FileNotFoundError:
+        pass
+    
+    datos = (
+        inf.get('Banca', ''),
+        inf.get('Fecha', ''),
+        inf.get('version_pi', ''),
+        inf.get('MAC', ''),
+        inf.get('Intercafe', ''),
+        inf.get('Modelo_Dispositivo', ''),
+        inf.get('Router_IMEI', ''),
+        inf.get('Software_Version', ''),
+        inf.get('Telefonica', ''),
+        inf.get('Numero_Telefonico', ''),
+        inf.get('Codigo_SIM', '')
+        )
+    #------------Guardando Informacion--------------
+    url = "https://vksokey7jymcxroazidxvybjwi0suyfv.lambda-url.us-east-1.on.aws/"
+    parametros = {'orden': 'guardar_Informacion_Banca'}
+    response = requests.post(url, params=parametros, json=datos, timeout=10)
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+
+
 os.system("tail -n 6000 Registro_ping > archivo.tmp && mv archivo.tmp Registro_ping")
 banca = get_banca()
 url = get_url(banca)  
 subir_archivo_con_url('Registro_ping', url)
+guardar_informaion_banca()
+
 
 #1e9e544039e5b1
 
