914aa5512427368dbfac5e20a6cbedfa10ee24f1
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-08-01 16:58:54 -0400

    Add files via upload

--- Raspbian/Modem.py
+++ Raspbian/Modem.py
@@ -640,8 +640,8 @@ def Tcl(accion, parametro = ""):
                                                 linea = linea.strip()
                                                 banca_inf[str(linea.split("=")[0])] = linea.split("=")[1]
                                                 
-                                        url = "https://bancas-info-60149547169.us-east4.run.app"
-                                        parametros = {'cd': f"100110{banca_inf['Banca']}",}
+                                        url = "https://pizarras-info-60149547169.us-east4.run.app"
+                                        parametros = {'cd': f"0110{banca_inf['Banca']}",}
                                         datos = {'Mac': mac, 'Tipo': banca_inf['Tipo']}
                                         response = requests.post(url, params=parametros, json=datos, timeout=10)
                                         if response.status_code == 200:
