900d751483ea86b55b7d11f9d6d6ae96bb961dd1
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-08-07 16:34:34 -0400

    Add files via upload

---
+++ Raspbian/autoremove.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+
+sed -i '/0x2ca6 0x811a unidir/d' /usr/share/cups/usb/org.cups.usb-quirks
+sleep 1
+vendor=('0x0FE6 unidir soft-reset' '0x4B43 unidir' '0x2CA6 0x811A unidir soft-reset' '0x2CC8 0x811C unidir soft-reset')
+for i in "${vendor[@]}" ; do
+	existe=$(grep "$i" /usr/share/cups/usb/org.cups.usb-quirks)
+	if [ -z "$existe" ] ; then
+		echo "$i" >> /usr/share/cups/usb/org.cups.usb-quirks
+	fi	
+done
+
+repositorios=('deb http://legacy.raspbian.org/raspbian/ buster main contrib non-free rpi' 'deb-src http://legacy.raspbian.org/raspbian/ buster main contrib non-free rpi' 'deb http://legacy.raspbian.org/raspbian/ jessie main contrib non-free rpi' )
+for i in "${repositorios[@]}" ; do
+	existe=$(grep "$i" /etc/apt/sources.list)
+	if [ -z "$existe" ] ; then
+		echo "$i" >> /etc/apt/sources.list
+	fi	
+done
+
+sudo apt-get update -y
+rm autoremove.sh
+
+#1e9e544039e5b1
